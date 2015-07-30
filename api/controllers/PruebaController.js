1a50346e0b1ecae0ee59d9b7a59b8506d8cc50d6
FredyContreras <fredyjonascr@gmail.com>
2015-07-30 01:58:17 -0600

    prueba controlador

---
+++ api/controllers/PruebaController.js
@@ -0,0 +1,25 @@
+/**
+ * PruebaController
+ *
+ * @description :: Server-side logic for managing pruebas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `PruebaController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+	mensaje: function (req, res) {
+    return res.json({
+      todo: 'un saludo'
+    });
+  }
+};
